f345a2598aeef42c069adb1441cc0ab61fdfe5c5
tcherevyk <tcherevyk@playtika.com>
2017-10-03 19:01:19 +0300

    refactored homework2, added homework3

--- src/com/playtika/second/Text.java
+++ src/com/playtika/second/Text.java
@@ -4,66 +4,50 @@ import java.util.*;
 
 class Text {
     private final String text;
-    private final ArrayList<String> textList;
 
     Text(String text) {
-        this.text = text;
         if (text == null) {
-            textList = null;
-            return;
-        }
-        //String[] textWithOnlyWords = text.trim().replaceAll("[^a-zA-Z0-9 ]", "").toLowerCase().split("\\s+");
-        String[] textWithOnlyWords = text.toLowerCase().split("[^a-zA-Z0-9]+");
-        this.textList = new ArrayList<> (Arrays.asList(textWithOnlyWords));
-        if (this.textList.size()>0 && this.textList.get(0).equals("")) {
-            this.textList.remove(0);
+            throw new IllegalArgumentException("Text cannot be null");
+        } else {
+            this.text = text;
         }
     }
 
-    public Set<String> getTopWords (int N) {
-        if (N < 1) {
+    public List<String> getTopWords(int n) {
+        if (n < 1) {
             throw new IllegalArgumentException("N should be positive");
         }
-        if (text == null) {
-            return null;
-        }
         if (text.isEmpty()) {
-            return new TreeSet<>();
+            return new ArrayList<>();
         }
-        TreeSet<String> wordsSet = new TreeSet<>(textList);
-        if (N < wordsSet.size()) {
-            List<String> topWordsList = (new LinkedList<>(wordsSet)).subList(0, N);
-            return new TreeSet<>(topWordsList);
-        } else {
-            return wordsSet;
-        }
+        Set<String> uniqueWords = new TreeSet<>(words());
+        return new ArrayList<>(uniqueWords).subList(0, Math.min(n, uniqueWords.size()));
     }
 
-    public Map<String,Integer> getFrequencies() {
-        if (text == null) {
-            return null;
+    public Map<String, Integer> getFrequencies() {
+        Map<String, Integer> frequencies = new HashMap<>();
+        for (String word : words()) {
+            Integer count = frequencies.get(word);
+            frequencies.put(word, count == null ? 1 : count + 1);
         }
-        HashMap<String,Integer> wordsFrequenciesMap = new HashMap<>();
-        for (String t : textList) {
-            if (wordsFrequenciesMap.containsKey(t)) {
-                Integer value = wordsFrequenciesMap.get(t);
-                wordsFrequenciesMap.put(t, ++value);
-            } else {
-                wordsFrequenciesMap.put(t, 1);
-            }
-        }
-        return wordsFrequenciesMap;
+        return frequencies;
     }
 
     public Integer getLengthInChars() {
-        if (text == null) {
-            return null;
-        }
         Integer sumOfChars = 0;
-        for (String s : textList) {
-            sumOfChars = sumOfChars + s.length();
+        for (String s : words()) {
+            sumOfChars += s.length();
         }
         return sumOfChars;
     }
 
+    private List<String> words() {
+        //String[] textWithOnlyWords = text.trim().replaceAll("[^a-zA-Z0-9 ]", "").toLowerCase().split("\\s+");
+        String[] textWithOnlyWords = text.toLowerCase().split("[^a-zA-Z0-9]+");
+        List<String> textList = new ArrayList<>(Arrays.asList(textWithOnlyWords));
+        if (textList.size() > 0 && textList.get(0).equals("")) {
+            textList.remove(0);
+        }
+        return textList;
+    }
 }
--- src/com/playtika/second/TextTest.java
+++ src/com/playtika/second/TextTest.java
@@ -1,76 +1,51 @@
 package com.playtika.second;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.TreeSet;
+import java.util.*;
 
+import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.*;
 
 public class TextTest {
     Text sentences = new Text("  one way,1 way -oNe*  ticket '");
-    Text nullText = new Text(null);
     Text emptyText = new Text(",  ^\\ @\n %\t ");
 
     @Test
     public void sortedSetOfUniqueWordsIsReturned() {
-        TreeSet<String> expectedSet = new TreeSet<>();
-        expectedSet.add("1");
-        expectedSet.add("one");
-        expectedSet.add("ticket");
-        expectedSet.add("way");
-
-        assertEquals(expectedSet.size(),sentences.getTopWords(4).size());
-        Iterator<String> iteratorExpected = expectedSet.iterator();
-        for (String nextActual : sentences.getTopWords(4)) {
-            String nextExpected = iteratorExpected.next();
-            assertEquals(nextExpected, nextActual);
-        }
+        assertThat(sentences.getTopWords(4), contains("1", "one", "ticket", "way"));
     }
 
     @Test
     public void wordsWithDifferentCaseAreTheSame() {
-        Text words = new Text("One wAy onE Way");
-        TreeSet<String> expectedSet = new TreeSet<>();
-        expectedSet.add("one");
-        expectedSet.add("way");
-        assertEquals(expectedSet.size(), words.getTopWords(4).size());
-        Iterator<String> iteratorExpected = expectedSet.iterator();
-        for (String nextActual : words.getTopWords(3)) {
-            String nextExpected = iteratorExpected.next();
-            assertEquals(nextExpected, nextActual);
-        }
+        assertThat(new Text("One wAy onE Way").getTopWords(4), contains("one", "way"));
     }
 
     @Test
     public void cutSetIsReturnedIfNLessThanSetSize() {
-        assertEquals(2,sentences.getTopWords(2).size());
+        assertThat(sentences.getTopWords(2), hasSize(2));
     }
 
     @Test
     public void fullSetIsReturnedIfNMoreThanSetSize() {
-        assertEquals(4,sentences.getTopWords(5).size());
+        assertThat(sentences.getTopWords(5), hasSize(4));
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void topCannotBeZero() throws Exception {
-        try {
-            sentences.getTopWords(0);
-            fail("IAE expected");
-        } catch (IllegalArgumentException e) {
-            assertEquals("N should be positive", e.getMessage());
-        }
+        sentences.getTopWords(0);
     }
 
     @Test
     public void topWordsIsEmptyForTextWithoutWords() {
-        assertEquals(0, emptyText.getTopWords(3).size());
-    }
-
-    @Test
-    public void topWordsIsNullForNullText() {
-        assertEquals(null, nullText.getTopWords(3));
+        assertThat(emptyText.getTopWords(3), empty());
     }
 
     @Test
@@ -78,34 +53,28 @@ public class TextTest {
         HashMap<String, Integer> expectedFrequencies = new HashMap<>();
         expectedFrequencies.put("one", 2);
         expectedFrequencies.put("way", 2);
-        expectedFrequencies.put("ticket",1);
+        expectedFrequencies.put("ticket", 1);
         expectedFrequencies.put("1", 1);
-        assertEquals(expectedFrequencies, sentences.getFrequencies());
+        assertThat(sentences.getFrequencies(), is(equalTo(expectedFrequencies)));
     }
 
     @Test
     public void wordFrequenciesIsEmptyForTextWithoutWords() {
-        HashMap<String, Integer> expectedFrequencies = new HashMap<>();
-        assertEquals(expectedFrequencies, emptyText.getFrequencies());
-    }
-
-    @Test
-    public void wordFrequenciesReturnsNullForNullText() {
-        assertEquals(null, nullText.getFrequencies());
+        assertThat(emptyText.getFrequencies().keySet(), hasSize(0));
     }
 
     @Test
     public void lengthInCharsIsReturnedForText() {
-        assertEquals(new Integer(19), sentences.getLengthInChars());
+        assertThat(sentences.getLengthInChars(), CoreMatchers.is(19));
     }
 
     @Test
     public void lengthIsZeroForTextWithoutWords() {
-        assertEquals(new Integer(0), emptyText.getLengthInChars());
+        assertThat(emptyText.getLengthInChars(), equalTo(0));
     }
 
-    @Test
-    public void lengthIsNullForNullText() {
-        assertEquals(null, nullText.getLengthInChars());
+    @Test(expected = IllegalArgumentException.class)
+    public void nullTextShouldNotBeProcessed() {
+        new Text(null);
     }
 }
